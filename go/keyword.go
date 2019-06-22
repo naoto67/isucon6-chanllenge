71cc67814def829a696cee2c9f2820aad60fdcb3
naoto67 <uver.com67@gmail.com>
2019-06-22 11:17:55 +0900

    update get keyword

--- go/keyword.go
+++ go/keyword.go
@@ -6,17 +6,17 @@ import (
 
 func getKeywords() ([]string, error) {
 	rows, err := db.Query(`
-		SELECT * FROM entry ORDER BY CHARACTER_LENGTH(keyword) DESC
+		SELECT keyword FROM entry ORDER BY CHARACTER_LENGTH(keyword) DESC
 	`)
 	panicIf(err)
 	keywords := make([]string, 0, 500)
 	for rows.Next() {
-		e := Entry{}
-		err := rows.Scan(&e.ID, &e.AuthorID, &e.Keyword, &e.Description, &e.UpdatedAt, &e.CreatedAt)
+		var keyword string
+		err := rows.Scan(&keyword)
 		if err != nil {
 			return keywords, err
 		}
-		keywords = append(keywords, regexp.QuoteMeta(e.Keyword))
+		keywords = append(keywords, regexp.QuoteMeta(keyword))
 	}
 	rows.Close()
 
