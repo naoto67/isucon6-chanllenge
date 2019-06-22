aa337bfe615f063412b88ee2225003d900c1f0dc
naoto67 <uver.com67@gmail.com>
2019-06-22 12:10:08 +0900

    update

--- go/util.go
+++ go/util.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/sha1"
 	"fmt"
 	"html"
 	"net/http"
@@ -66,17 +65,13 @@ func newHtmlify(w http.ResponseWriter, r *http.Request, content string, keywords
 		return ""
 	}
 	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
-	kw2sha := make(map[string]string)
 	content = re.ReplaceAllStringFunc(content, func(kw string) string {
-		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		return kw2sha[kw]
-	})
-	content = html.EscapeString(content)
-	for kw, hash := range kw2sha {
 		u, err := r.URL.Parse(baseUrl.String() + "/keyword/" + pathURIEscape(kw))
 		panicIf(err)
 		link := fmt.Sprintf("<a href=\"%s\">%s</a>", u, html.EscapeString(kw))
-		content = strings.Replace(content, hash, link, -1)
-	}
+		return link
+	})
+	content = html.EscapeString(content)
+
 	return strings.Replace(content, "\n", "<br />\n", -1)
 }
