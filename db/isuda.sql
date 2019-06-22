c9b2d7c3e37170ecc002a6d2daa31a1af83f887f
naoto67 <uver.com67@gmail.com>
2019-06-22 15:27:05 +0900

    update

--- db/isuda.sql
+++ db/isuda.sql
@@ -3,8 +3,10 @@ CREATE TABLE entry (
     author_id BIGINT UNSIGNED NOT NULL,
     keyword VARCHAR(191) UNIQUE,
     description MEDIUMTEXT,
+    len SMALLINT,
     updated_at DATETIME NOT NULL,
-    created_at DATETIME NOT NULL
+    created_at DATETIME NOT NULL,
+    KEY len_idx_on_entry(len)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE user (
@@ -21,3 +23,5 @@ CREATE TABLE star (
     user_name VARCHAR(191) NOT NULL,
     created_at DATETIME
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+UPDATE entry e1, entry e2 SET e1.len = character_length(e2.description) where e1.id = e2.id
