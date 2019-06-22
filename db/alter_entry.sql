e661d2c7fce292d00da0105068b5d26e2378902f
naoto67 <uver.com67@gmail.com>
2019-06-22 15:40:41 +0900

    fix

--- db/alter_entry.sql
+++ db/alter_entry.sql
@@ -1,4 +1,4 @@
-ALTER TABLE entry ADD len MIDIUMINT;
+ALTER TABLE entry ADD len MEDIUMINT;
 ALTER TABLE entry ADD INDEX len_idx_on_entry(len);
 
-UPDATE entry e1, entry e2 SET e1.len = character_length(e2.description) where e1.id = e2.id;
+UPDATE entry e1, entry e2 SET e1.len = character_length(e2.keyword) where e1.id = e2.id;
