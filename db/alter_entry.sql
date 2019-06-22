5583d6f12719ebe8baf6745514ae223d7c73590c
naoto67 <uver.com67@gmail.com>
2019-06-22 15:37:07 +0900

    update

--- db/alter_entry.sql
+++ db/alter_entry.sql
@@ -1,3 +1,4 @@
-ALTER TABLE entry ADD len SMALLINT;
+ALTER TABLE entry ADD len MIDIUMINT;
+ALTER TABLE entry ADD INDEX len_idx_on_entry(len);
 
 UPDATE entry e1, entry e2 SET e1.len = character_length(e2.description) where e1.id = e2.id;
